7e55b50d0e31b26855f87e5ae91ecc1f7ff2a098
AnnZhimol <ankavanka731@gmail.com>
2024-04-09 14:15:27 +0400

    add filter and dto short

--- src/main/java/com/example/consul/dto/WB_DetailReport.java
+++ src/main/java/com/example/consul/dto/WB_DetailReport.java
@@ -9,40 +9,40 @@ import lombok.Setter;
 @Setter
 @Data
 public class WB_DetailReport {
-    Integer realizationreport_id;
-    String date_from;
-    String date_to;
-    String create_dt;
-    String currency_name;
-    String suppliercontract_code;
-    Long rrd_id;
-    Integer gi_id;
+    Integer realizationreport_id; //не надо
+    String date_from; //не надо
+    String date_to; //не надо
+    String create_dt; //не надо
+    String currency_name; //не надо
+    String suppliercontract_code; //не надо
+    Long rrd_id; //не надо
+    Integer gi_id; //не надо
     String subject_name;
     Integer nm_id;
     String brand_name;
     String sa_name;
-    String ts_name;
-    String barcode;
+    String ts_name; //не надо
+    String barcode; //не надо
     String doc_type_name;
     Integer quantity;
     Double retail_price;
     Double retail_amount;
     Integer sale_percent;
     Double commission_percent;
-    String office_name;
+    String office_name; //не надо
     String supplier_oper_name;
-    String order_dt;
-    String sale_dt;
-    String rr_dt;
-    Long shk_id;
+    String order_dt; //не надо
+    String sale_dt; //не надо
+    String rr_dt; //не надо
+    Long shk_id; //не надо
     Double retail_price_withdisc_rub;
     Integer delivery_amount;
     Integer return_amount;
     Double delivery_rub;
-    String gi_box_type_name;
+    String gi_box_type_name; //не надо
     Double product_discount_for_report;
     Double supplier_promo;
-    Long rid;
+    Long rid; //не надо
     Double ppvz_spp_prc;
     Double ppvz_kvw_prc_base;
     Double ppvz_kvw_prc;
@@ -52,26 +52,26 @@ public class WB_DetailReport {
     Double ppvz_for_pay;
     Double ppvz_reward;
     Double acquiring_fee;
-    String acquiring_bank;
+    String acquiring_bank; //не надо
     Double ppvz_vw;
     Double ppvz_vw_nds;
-    Integer ppvz_office_id;
-    String ppvz_office_name;
-    Integer ppvz_supplier_id;
-    String ppvz_supplier_name;
-    String ppvz_inn;
+    Integer ppvz_office_id; //не надо
+    String ppvz_office_name; //не надо
+    Integer ppvz_supplier_id; //не надо
+    String ppvz_supplier_name; //не надо
+    String ppvz_inn; //не надо
     String declaration_number;
     String bonus_type_name;
     String sticker_id;
-    String site_country;
+    String site_country; //не надо
     Double penalty;
     Double additional_payment;
     Double rebill_logistic_cost;
-    String rebill_logistic_org;
+    String rebill_logistic_org; //не надо
     String kiz;
     Double storage_fee;
     Double deduction;
     Double acceptance;
-    String srid;
+    String srid; //не надо
     Integer report_type;
 }
---
+++ src/main/java/com/example/consul/dto/WB_DetailReportShort.java
@@ -0,0 +1,133 @@
+package com.example.consul.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+public class WB_DetailReportShort {
+    String subject_name;
+    Integer nm_id;
+    String brand_name;
+    String sa_name;
+    String doc_type_name;
+    Integer quantity;
+    Double retail_price;
+    Double retail_amount;
+    Integer sale_percent;
+    Double commission_percent;
+    String supplier_oper_name;
+    Double retail_price_withdisc_rub;
+    Integer delivery_amount;
+    Integer return_amount;
+    Double delivery_rub;
+    Double product_discount_for_report;
+    Double supplier_promo;
+    Double ppvz_spp_prc;
+    Double ppvz_kvw_prc_base;
+    Double ppvz_kvw_prc;
+    Double sup_rating_prc_up;
+    Double is_kgvp_v2;
+    Double ppvz_sales_commission;
+    Double ppvz_for_pay;
+    Double ppvz_reward;
+    Double acquiring_fee;
+    Double ppvz_vw;
+    Double ppvz_vw_nds;
+    String declaration_number;
+    String bonus_type_name;
+    String sticker_id;
+    Double penalty;
+    Double additional_payment;
+    Double rebill_logistic_cost;
+    String kiz;
+    Double storage_fee;
+    Double deduction;
+    Double acceptance;
+    Integer report_type;
+
+    public WB_DetailReportShort(String subject_name,
+                                Integer nm_id,
+                                String brand_name,
+                                String sa_name,
+                                String doc_type_name,
+                                Integer quantity,
+                                Double retail_price,
+                                Double retail_amount,
+                                Integer sale_percent,
+                                Double commission_percent,
+                                String supplier_oper_name,
+                                Double retail_price_withdisc_rub,
+                                Integer delivery_amount,
+                                Integer return_amount,
+                                Double delivery_rub,
+                                Double product_discount_for_report,
+                                Double supplier_promo,
+                                Double ppvz_spp_prc,
+                                Double ppvz_kvw_prc_base,
+                                Double ppvz_kvw_prc,
+                                Double sup_rating_prc_up,
+                                Double is_kgvp_v2,
+                                Double ppvz_sales_commission,
+                                Double ppvz_for_pay,
+                                Double ppvz_reward,
+                                Double acquiring_fee,
+                                Double ppvz_vw,
+                                Double ppvz_vw_nds,
+                                String declaration_number,
+                                String bonus_type_name,
+                                String sticker_id,
+                                Double penalty,
+                                Double additional_payment,
+                                Double rebill_logistic_cost,
+                                String kiz,
+                                Double storage_fee,
+                                Double deduction,
+                                Double acceptance,
+                                Integer report_type) {
+    }
+
+    public static WB_DetailReportShort of(WB_DetailReport wb_detailReport) {
+        return new WB_DetailReportShort(wb_detailReport.getSubject_name(),
+                wb_detailReport.getNm_id(),
+                wb_detailReport.getBrand_name(),
+                wb_detailReport.getSa_name(),
+                wb_detailReport.getDoc_type_name(),
+                wb_detailReport.getQuantity(),
+                wb_detailReport.getRetail_price(),
+                wb_detailReport.getRetail_amount(),
+                wb_detailReport.getSale_percent(),
+                wb_detailReport.getCommission_percent(),
+                wb_detailReport.getSupplier_oper_name(),
+                wb_detailReport.getRetail_price_withdisc_rub(),
+                wb_detailReport.getDelivery_amount(),
+                wb_detailReport.getReturn_amount(),
+                wb_detailReport.getDelivery_rub(),
+                wb_detailReport.getProduct_discount_for_report(),
+                wb_detailReport.getSupplier_promo(),
+                wb_detailReport.getPpvz_spp_prc(),
+                wb_detailReport.getPpvz_kvw_prc_base(),
+                wb_detailReport.getPpvz_kvw_prc(),
+                wb_detailReport.getSup_rating_prc_up(),
+                wb_detailReport.getIs_kgvp_v2(),
+                wb_detailReport.getPpvz_sales_commission(),
+                wb_detailReport.getPpvz_for_pay(),
+                wb_detailReport.getPpvz_reward(),
+                wb_detailReport.getAcquiring_fee(),
+                wb_detailReport.getPpvz_vw(),
+                wb_detailReport.getPpvz_vw_nds(),
+                wb_detailReport.getDeclaration_number(),
+                wb_detailReport.getBonus_type_name(),
+                wb_detailReport.getSticker_id(),
+                wb_detailReport.getPenalty(),
+                wb_detailReport.getAdditional_payment(),
+                wb_detailReport.getRebill_logistic_cost(),
+                wb_detailReport.getKiz(),
+                wb_detailReport.getStorage_fee(),
+                wb_detailReport.getDeduction(),
+                wb_detailReport.getAcceptance(),
+                wb_detailReport.getReport_type());
+    }
+}
