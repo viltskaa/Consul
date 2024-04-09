a8a79041d91d6885c2d48dae2cc5335d363e9ba0
andrew <andrewstyou@gmail.com>
2024-04-09 11:31:12 +0400

    add SpringSecurityConfig

---
+++ src/main/java/com/example/consul/security/SpringSecurityConfig.java
@@ -0,0 +1,6 @@
+package com.example.consul.security;
+
+import org.springframework.boot.autoconfigure.security.SecurityDataConfiguration;
+
+public class SpringSecurityConfig extends SecurityDataConfiguration {
+}
