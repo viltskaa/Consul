513165e0783b17a089d98587e4e55cb0bad4e6d4
andrew <andrewstyou@gmail.com>
2024-04-09 11:31:37 +0400

    some fixes - rename methods

--- src/main/java/com/example/consul/controllers/WB_AdReportController.java
+++ src/main/java/com/example/consul/controllers/WB_AdReportController.java
@@ -2,7 +2,6 @@ package com.example.consul.controllers;
 
 import com.example.consul.dto.WB_AdReport;
 import com.example.consul.services.WB_Service;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +17,7 @@ public class WB_AdReportController {
 
     public WB_AdReportController(WB_Service wbService) {
         this.wbService = wbService;
-        wbService.setApi("eyJhbGciOiJFUzI1NiIsImtpZCI6IjIwMjQwMjI2djEiLCJ0eXAiOiJKV1QifQ.eyJlbnQiOjEsImV4cCI6MTcyNzgxMzkyMSwiaWQiOiIwYTY5NDVkZS0wODQyLTQ1ZmItOGEyMC0zNTMzNzliYjk1NjUiLCJpaWQiOjQ1ODkwNDkwLCJvaWQiOjg5NzE2NiwicyI6MTA3Mzc0MjMzNCwic2lkIjoiMTZhMGZiZWEtYWVmZi00YjgxLThmNzEtZjYyZDlhYjJmMGM1IiwidCI6ZmFsc2UsInVpZCI6NDU4OTA0OTB9.DtgYFO1TioCOeKiKI4VKw0_QbD8S4908JSxj2196k_pUDH1vgNBiUPImwWMGhaDgpE8GVEdzOQnPj23aiRT4JQ");
+        wbService.setApiKey("eyJhbGciOiJFUzI1NiIsImtpZCI6IjIwMjQwMjI2djEiLCJ0eXAiOiJKV1QifQ.eyJlbnQiOjEsImV4cCI6MTcyNzgxMzkyMSwiaWQiOiIwYTY5NDVkZS0wODQyLTQ1ZmItOGEyMC0zNTMzNzliYjk1NjUiLCJpaWQiOjQ1ODkwNDkwLCJvaWQiOjg5NzE2NiwicyI6MTA3Mzc0MjMzNCwic2lkIjoiMTZhMGZiZWEtYWVmZi00YjgxLThmNzEtZjYyZDlhYjJmMGM1IiwidCI6ZmFsc2UsInVpZCI6NDU4OTA0OTB9.DtgYFO1TioCOeKiKI4VKw0_QbD8S4908JSxj2196k_pUDH1vgNBiUPImwWMGhaDgpE8GVEdzOQnPj23aiRT4JQ");
     }
 
     @GetMapping(path = "/get")
