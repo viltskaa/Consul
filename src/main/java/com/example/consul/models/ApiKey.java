9d3781efa02b99f277fa96287dc718b859be778a
andrew <andrewstyou@gmail.com>
2024-04-09 12:03:55 +0400

    add models

--- src/main/java/com/example/consul/models/ApiKey.java
+++ src/main/java/com/example/consul/models/ApiKey.java
@@ -1,10 +1,15 @@
 package com.example.consul.models;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 public class ApiKey {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Integer id;
+    private String key;
 }
