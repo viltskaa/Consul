d163f165bf0fe41bd5445dda460ba849a415b921
andrew <andrewstyou@gmail.com>
2024-04-09 11:31:19 +0400

    init repositories

---
+++ src/main/java/com/example/consul/repositories/ApiKeyRepository.java
@@ -0,0 +1,5 @@
+package com.example.consul.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApiKeyRepository extends JpaRepository<Object, Integer> {
+}
