aa47c2014f476addc35bd4792efd5b97d6cdaf62
Hells Hound <irisha1234517@gmail.com>
2024-04-23 18:43:08 +0400

    тест getProductInfo

--- src/test/java/com/example/consul/ConsulApplicationTests.java
+++ src/test/java/com/example/consul/ConsulApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.consul;
 import com.example.consul.api.OZON_Api;
 import com.example.consul.document.Excel;
 import com.example.consul.dto.OZON.OZON_DetailReport;
+import com.example.consul.dto.OZON.OZON_SkuProductsReport;
 import com.example.consul.mapping.ListToHtml;
 import com.example.consul.models.ApiKey;
 import org.junit.jupiter.api.Test;
@@ -10,13 +11,28 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.List;
 
 @SpringBootTest
 class ConsulApplicationTests {
 
+
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void getProductInfo(){
+        final OZON_Api api = new OZON_Api();
+        api.setHeaders("ace0b5ec-e3f6-4eb4-a9a6-33a1a5c84f66", "350423");
+        Collection<Long> skus = new ArrayList<>();
+        skus.add(477040103L);
+        skus.add(477056233L);
+
+        OZON_SkuProductsReport report = api.getProductInfo(skus);
+        System.out.println(report.findBySku(477040103L).getOffer_id());
     }
 
     @Test
@@ -47,4 +63,5 @@ class ConsulApplicationTests {
         Excel excel = new Excel();
         excel.createExcel("test.xls");
     }
+
 }
