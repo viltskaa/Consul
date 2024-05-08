5329f4bb8249db4d4ad8cd7fde67bb22b97fc8df
andrew <andrewstyou@gmail.com>
2024-05-08 17:49:44 +0400

    refactor detail report

--- src/main/java/com/example/consul/dto/OZON/OZON_DetailReport.java
+++ src/main/java/com/example/consul/dto/OZON/OZON_DetailReport.java
@@ -1,130 +1,81 @@
 package com.example.consul.dto.OZON;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.util.List;
 
-@Getter
-@Setter
 @Data
 public class OZON_DetailReport {
-    Result result;
+    private Result result;
 
-    public OZON_DetailReport(Result result) {
-        this.result = result;
+    @Data
+    public static class Result {
+        private Header header;
+        private List<Row> rows;
     }
 
     @Data
-    public static class Result{
-        Header header;
-        List<Row> rows;
-
-        public Result(Header header, List<Row> rows) {
-            this.header = header;
-            this.rows = rows;
-        }
+    private static class Header {
+        private String doc_date;
+        private String number;
+        private String start_date;
+        private String stop_date;
+        private String contract_date;
+        private String contract_number;
+        private String payer_name;
+        private String payer_inn;
+        private String payer_kpp;
+        private String receiver_name;
+        private String receiver_inn;
+        private String receiver_kpp;
+        private Double doc_amount;
+        private Double vat_amount;
+        private String currency_sys_name;
     }
 
     @Data
-    public static class Header{
-        String doc_date;
-        String number;
-        String start_date;
-        String stop_date;
-        String contract_date;
-        String contract_number;
-        String payer_name;
-        String payer_inn;
-        String payer_kpp;
-        String receiver_name;
-        String receiver_inn;
-        String receiver_kpp;
-        Double doc_amount;
-        Double vat_amount;
-        String currency_sys_name;
-
-        public Header(String doc_date, String num, String start_date, String stop_date, String contract_date, String contract_num, String payer_name, String payer_inn, String payer_kpp, String rcv_name, String rcv_inn, String rcv_kpp, Double doc_amount, Double vat_amount, String currency_code) {
-            this.doc_date = doc_date;
-            this.number = num;
-            this.start_date = start_date;
-            this.stop_date = stop_date;
-            this.contract_date = contract_date;
-            this.contract_number = contract_num;
-            this.payer_name = payer_name;
-            this.payer_inn = payer_inn;
-            this.payer_kpp = payer_kpp;
-            this.receiver_name = rcv_name;
-            this.receiver_inn = rcv_inn;
-            this.receiver_kpp = rcv_kpp;
-            this.doc_amount = doc_amount;
-            this.vat_amount = vat_amount;
-            this.currency_sys_name = currency_code;
-        }
+    public static class DeliveryCommission {
+        private Double amount;
+        private Double bonus;
+        private Double commission;
+        private Double compensation;
+        private Double price_per_instance;
+        private Integer quantity;
+        private Double standard_fee;
+        private Double stars;
+        private Double total;
     }
 
     @Data
-    public static class DeliveryCommission{
-        Double amount;
-        Double bonus;
-        Double commission;
-        Double compensation;
-        Double price_per_instance;
-        Integer quantity;
-        Double standard_fee;
-        Double stars;
-        Double total;
+    public static class Item {
+        private String barcode;
+        private String name;
+        private String offer_id;
+        private Long sku;
     }
 
     @Data
-    public static class Item{
-        String barcode;
-        String name;
-        String offer_id;
-        Long sku;
+    public static class ReturnCommission {
+        private Double amount;
+        private Double bonus;
+        private Double commission;
+        private Double compensation;
+        private Double price_per_instance;
+        private Integer quantity;
+        private Double standard_fee;
+        private Double stars;
+        private Double total;
     }
 
     @Data
-    public static class ReturnCommission{
-        Double amount;
-        Double bonus;
-        Double commission;
-        Double compensation;
-        Double price_per_instance;
-        Integer quantity;
-        Double standard_fee;
-        Double stars;
-        Double total;
-    }
-
-    @Data
-    public static class Row{
-        Double commission_ratio;
-        DeliveryCommission delivery_commission;
-        Item item;
-        ReturnCommission return_commission;
-        Integer rowNumber;
-        Double seller_price_per_instance;
-
-        public Row(Double commission_ratio, DeliveryCommission delivery_commission, Item item, ReturnCommission return_commission, Integer rowNumber, Double seller_price_per_instance) {
-            this.commission_ratio = commission_ratio;
-            this.delivery_commission = delivery_commission;
-            this.item = item;
-            this.return_commission = return_commission;
-            this.rowNumber = rowNumber;
-            this.seller_price_per_instance = seller_price_per_instance;
-        }
-
-
-        public static Row of(Row row) {
-            return new Row(
-                    row.getCommission_ratio(),
-                    row.getDelivery_commission(),
-                    row.getItem(),
-                    row.getReturn_commission(),
-                    row.getRowNumber(),
-                    row.getSeller_price_per_instance());
-        }
+    @AllArgsConstructor
+    public static class Row {
+        private Double commission_ratio;
+        private DeliveryCommission delivery_commission;
+        private Item item;
+        private ReturnCommission return_commission;
+        private Integer rowNumber;
+        private Double seller_price_per_instance;
     }
 }
