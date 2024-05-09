fe2e7ec23b2c7853099e9f900f344da47f92fe99
AnnZhimol <ankavanka731@gmail.com>
2024-05-10 00:55:25 +0400

    add YANDEX_dataProcessing

--- src/test/java/com/example/consul/YandexMarketTests.java
+++ src/test/java/com/example/consul/YandexMarketTests.java
@@ -1,15 +1,12 @@
 package com.example.consul;
 
 import com.example.consul.api.YANDEX_Api;
+import com.example.consul.mapping.YANDEX_dataProcessing;
 import joinery.DataFrame;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.junit.jupiter.api.Test;
 
 import java.net.MalformedURLException;
-import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
 import java.net.URL;
 import java.nio.channels.Channels;
@@ -17,7 +14,6 @@ import java.nio.channels.ReadableByteChannel;
 import java.util.ArrayList;
 
 import java.io.*;
-import java.util.List;
 
 @SpringBootTest
 class YandexMarketTests {
@@ -53,37 +49,12 @@ class YandexMarketTests {
 
     @Test
     public void readingYandexXls() throws IOException {
-        File file = new File("statistics-report-2024-01.xls");
-        Workbook wb = WorkbookFactory.create(file);
-        Sheet sheet = wb.getSheetAt(1);
-        List<String> listHeader = new ArrayList<>();
-        List<Object> listData = new ArrayList<>();
+        DataFrame<Object> df1 = YANDEX_dataProcessing.getDataFromSheet("statistics-report-2024-01.xls",4);
+        DataFrame<Object> df2 = YANDEX_dataProcessing.getDataFromSheet("statistics-report-2024-01.xls",2);
+        DataFrame<Object> df3 = YANDEX_dataProcessing.getDataFromSheet("1eff27ea-100f-4870-b5b6-ffea4b4f49b4.xls",2);
 
-        for(int i = 0;i < sheet.getRow(13).getPhysicalNumberOfCells(); i++){
-            listHeader.add(sheet.getRow(13).getCell(i).getStringCellValue());
-        }
-
-        DataFrame<Object> df = new DataFrame<>(listHeader);
-
-        for(int j = 14; j < sheet.getPhysicalNumberOfRows() - 1; j++){
-            for(int i = 0; i < sheet.getRow(j).getPhysicalNumberOfCells(); i++){
-                switch (sheet.getRow(j).getCell(i).getCellType()) {
-                    case BOOLEAN:
-                        listData.add(sheet.getRow(j).getCell(i).getBooleanCellValue());
-                        break;
-                    case NUMERIC:
-                        listData.add(sheet.getRow(j).getCell(i).getNumericCellValue());
-                        break;
-                    case STRING:
-                        listData.add(sheet.getRow(j).getCell(i).getRichStringCellValue());
-                        break;
-                }
-            }
-            df.append(listData);
-            listData = new ArrayList<>();
-        }
-
-        System.out.println(df);
-
+        System.out.println(YANDEX_dataProcessing.getReturnData(df1));
+        System.out.println(YANDEX_dataProcessing.getDeliveredData(df2));
+        System.out.println(YANDEX_dataProcessing.getFavorData(df3,df2));
     }
 }
